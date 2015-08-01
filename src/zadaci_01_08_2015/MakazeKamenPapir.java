5f357d7184fcb43a2afd84580acc0c1cc9361a1d
djomla79 <tm.djomla79@gmail.com>
2015-08-01 16:13:09 +0200

    Zadaci

--- src/zadaci_01_08_2015/MakazeKamenPapir.java
+++ src/zadaci_01_08_2015/MakazeKamenPapir.java
@@ -11,24 +11,31 @@ public class MakazeKamenPapir {
 		 Program pita korisnika da unese 0, 1 ili 2 te mu ispisuje poruku da li je korisnik pobijedio,
 		 da li je raèunar pobijedio ili je bilo neriješeno.
 		 */
+		Scanner input = new Scanner(System.in);
 		
 		int broj = (int)(Math.random()*3);              // generisanje nasumicnog broja 0, 1 ili 2
 		
-		Scanner input = new Scanner(System.in);
+		String [] mkp = {"Makaze", "Kamen", "Papir"};   // string koji sadrzi makaze, kamen i papir
 				
-		System.out.print("Unesite broj 0, 1 ili 2: ");
+		System.out.println("Unesite broj 0, 1 ili 2");
+		System.out.print("(Makaze=0, Kamen=1, Papir=2): ");
 				
 		int MKP = input.nextInt();                      // unos brojeva 0, 1 ili 2
-				
-				                                       // kombinacije brojeva 0, 1, i 2 gdje su makaze=0, kamen=1 i papir=2, 
+		while(MKP < 0 || MKP > 2) {                     // dok unos ne odgovara
+			System.out.print("Unesite broj 0, 1 ili 2 ponovo: ");
+			MKP = input.nextInt();                      // unos se vrsi ponovo
+		}
+		for(int i=0; i<mkp.length; i++) {               // petlja koja prolazi kroz string koji sadrzi makaze, kamen i papir
+		}
+		System.out.println();
 		if (broj==0 && MKP==0 || broj==1 && MKP==1 || broj==2 && MKP==2){  // prvi uslov, rezultat nerijesen
-			System.out.print("Kompjuter je dobio: " + broj + " a vi ste dobili: " + MKP + "\nRezultat je nerijesen!");
+			System.out.print("Kompjuter je dobio: " + mkp[broj] + "\nVi ste dobili: " + mkp[MKP] + "\nRezultat je nerijesen!");
 		}
 		if (broj==0 && MKP==2 || broj==1 && MKP==0 || broj==2 && MKP==1){  // drugi uslov, kompjuter pobjednik
-			System.out.print("Kompjuter je dobio: " + broj + " a vi ste dobili: " + MKP + "\nKompjuter je pobjednik!");
+			System.out.print("Kompjuter je dobio: " + mkp[broj] + "\nVi ste dobili: " + mkp[MKP] + "\nKompjuter je pobjednik!");
 		}
 		if (broj==1 && MKP==2 || broj==0 && MKP==1 || broj==2 && MKP==0){  // treci uslov, igrac pobjednik
-			System.out.print("Kompjuter je dobio: " + broj + " a vi ste dobili: " + MKP + "\nVi ste pobjednik!");
+			System.out.print("Kompjuter je dobio: " + mkp[broj] + " a vi ste dobili: " + mkp[MKP] + "\nVi ste pobjednik!");
 		}
 		input.close();
 	}
