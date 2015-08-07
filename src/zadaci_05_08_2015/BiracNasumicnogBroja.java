f207447d619a1deb154bc01469b9035e069d337e
djomla79 <tm.djomla79@gmail.com>
2015-08-07 19:57:46 +0200

    Zadacu

--- src/zadaci_05_08_2015/BiracNasumicnogBroja.java
+++ src/zadaci_05_08_2015/BiracNasumicnogBroja.java
@@ -10,22 +10,23 @@ public class BiracNasumicnogBroja {
 		public static int getRandom(int... numbers)
         */
 		
-		int [] numbers = {5, 8, 12, 1, 3, 9, 2, 13, 45, 36};
+		int [] numbers = {5, 8, 12, 1, 3, 9, 2, 13, 45, 36}; // proizvoljan niz brojeva
 		
-		getRandom(numbers);
+		getRandom(numbers);          // pozivanje metode
 		
 	}
-	
+	/** Metoda koja prima niz brojeva i vraca nasumicni broj */
 	public static int getRandom(int...numbers) {
 			
-		int nasumicni = (int)(Math.random()*54+1);
+		int nasumicni = (int)(Math.random()*54+1); // nasumican broj izmedju 1 i 54
 		int number = 0;
 		    
-		for (int n: numbers) {
-			if (n != nasumicni) {
-				number = nasumicni;
+		for (int n: numbers) {      // za sve elemente niza
+			if (n != nasumicni) {   // ako su brojevi u nizu razliciti od nasumicnog
+				number = nasumicni; // odredjujemo nasumicni
 			}
 		}
+		                            // i vrsimo njegov ispis u konzolu
 		System.out.println("Nasumicni broj je: " + number);
 		return number;
 	}
