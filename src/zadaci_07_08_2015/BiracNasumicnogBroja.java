7b9cd9344664700a10e86cb7cd970ed81bfbf814
djomla79 <tm.djomla79@gmail.com>
2015-08-07 20:19:45 +0200

    Zadaci

---
+++ src/zadaci_07_08_2015/BiracNasumicnogBroja.java
@@ -0,0 +1,43 @@
+package zadaci_07_08_2015;
+
+public class BiracNasumicnogBroja {
+
+	public static void main(String[] args) {
+		/*
+		(Random number chooser) Write a method that returns a random number between
+		1 and 54, excluding the numbers passed in the argument. The method header is
+		specified as follows:
+		public static int getRandom(int... numbers)
+        */
+		
+		int [] numbers = {5, 8, 12, 1, 3, 9, 2, 13, 45, 36}; // proizvoljan niz brojeva
+		
+		getRandom(numbers);             // pozivanje metode
+		
+	}
+	/** Metoda koja prima niz brojeva i vraca nasumicni broj */
+	public static int getRandom(int...numbers) {
+		
+		boolean isOn = true;
+		int number = 0;
+			
+		while(isOn) {
+		
+			int nasumicni = (int)(Math.random()*54+1); // nasumican broj izmedju 1 i 54
+		
+			for (int i=0; i<numbers.length; i++) {      // za sve elemente niza
+				
+				if (numbers[i] == nasumicni) {   // ako je neki od brojeva iz niza isti sa nasumicnim
+					isOn = true;                 // glavna petlja nastavlja sa radom
+					break;                       // prekid for petlje
+				} else {                         // ako nema isti zajednicki broj
+					number = nasumicni;          // number postaje nasumicni broj
+					isOn = false;                // glavna petlja prestaje sa radom
+				}
+			}
+		}
+		                                // i vrsimo njegov ispis u konzolu
+		System.out.println("Nasumicni broj je: " + number);
+		return number;
+		}
+	}
---
+++ src/zadaci_07_08_2015/MatricaSabiranjeKolona.java
@@ -0,0 +1,56 @@
+package zadaci_07_08_2015;
+
+import java.util.Scanner;
+
+public class MatricaSabiranjeKolona {
+
+	public static void main(String[] args) {
+		/*
+		 (Sum elements column by column) Write a method that returns the sum of all the
+		 elements in a specified column in a matrix using the following header:
+		 
+		 public static double sumColumn(double[][] m, int columnIndex)
+		 
+		 Write a test program that reads a 3-by-4 matrix and displays the sum of each column
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Unesite broj redova i broj kolona niza: ");
+		
+		int redovi = input.nextInt();
+		int kolone = input.nextInt();          // unos redova i kolona matrice
+		
+		int [][] matrix = new int [redovi][kolone];
+		
+        System.out.print("Unesite " + redovi + " reda i " + kolone + " kolone: ");
+		
+		for (int i=0; i<matrix.length; i++) { // unos elemenata u matricu
+			for (int j=0; j<matrix[i].length; j++) {
+				matrix[i][j] = input.nextInt();
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	
+		System.out.print("Unesite broj index-a kolone: ");
+	
+		int kolona = input.nextInt();         // unos indexa kolone
+	
+		sumaKolona(matrix, kolona);           // pozivanje metode
+	
+		input.close();
+
+	}
+	/** Metoda koja prima argumente matricu i integer i vraca sumu kolone matrice */
+	public static void sumaKolona(int[][] niz, int index) {
+	
+		int suma = 0;
+	
+		for (int i=0; i<niz.length; i++) {
+			suma += niz[i][index-1];          // sabiranje elemenata zeljene kolone u matrici
+		}
+		                                      // ispis ukupnog zbira kolone za uneseni index kolone
+		System.out.println("Ukupan zbir svih elemenata " + index + ". kolone je: " + suma);
+	}	
+}
