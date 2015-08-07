13fe0060290e6727883990e5daabff1832a65347
djomla79 <tm.djomla79@gmail.com>
2015-08-07 20:24:01 +0200

    Zadaci

--- src/zadaci_05_08_2015/BiracNasumicnogBroja.java
+++ src/zadaci_05_08_2015/BiracNasumicnogBroja.java
@@ -12,22 +12,32 @@ public class BiracNasumicnogBroja {
 		
 		int [] numbers = {5, 8, 12, 1, 3, 9, 2, 13, 45, 36}; // proizvoljan niz brojeva
 		
-		getRandom(numbers);          // pozivanje metode
+		getRandom(numbers);             // pozivanje metode
 		
 	}
 	/** Metoda koja prima niz brojeva i vraca nasumicni broj */
 	public static int getRandom(int...numbers) {
-			
-		int nasumicni = (int)(Math.random()*54+1); // nasumican broj izmedju 1 i 54
+		
+		boolean isOn = true;
 		int number = 0;
-		    
-		for (int n: numbers) {      // za sve elemente niza
-			if (n != nasumicni) {   // ako su brojevi u nizu razliciti od nasumicnog
-				number = nasumicni; // odredjujemo nasumicni
+			
+		while(isOn) {
+		
+			int nasumicni = (int)(Math.random()*54+1); // nasumican broj izmedju 1 i 54
+		
+			for (int i=0; i<numbers.length; i++) {      // za sve elemente niza
+				
+				if (numbers[i] == nasumicni) {   // ako je neki od brojeva iz niza isti sa nasumicnim
+					isOn = true;                 // glavna petlja nastavlja sa radom
+					break;                       // prekid for petlje
+				} else {                         // ako nema isti zajednicki broj
+					number = nasumicni;          // number postaje nasumicni broj
+					isOn = false;                // glavna petlja prestaje sa radom
+				}
 			}
 		}
-		                            // i vrsimo njegov ispis u konzolu
+		                                // i vrsimo njegov ispis u konzolu
 		System.out.println("Nasumicni broj je: " + number);
 		return number;
+		}
 	}
-}
