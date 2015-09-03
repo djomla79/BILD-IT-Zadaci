12c67f55745239fe01fbef76e874494520c06db5
djomla79 <tm.djomla79@gmail.com>
2015-09-03 10:16:32 +0200

    Zadaci

--- src/zadaci_02_09_2015_Opcionalni/SumaBrojevaRekurzija.java
+++ src/zadaci_02_09_2015_Opcionalni/SumaBrojevaRekurzija.java
@@ -6,37 +6,40 @@ public class SumaBrojevaRekurzija {
 
 	public static void main(String[] args) {
 		/*
-		 * (Sum the digits in an integer using recursion)
-		 * Write a recursive method that computes the sum
-		 * of the digits in an integer.
-		 * Use the following method header:
-		 * 
-		 * public static int sumDigits(long n)
-		 * 
-		 * For example, sumDigits(234) returns 2 + 3 + 4 = 9.
-		 * Write a test program that prompts the user
-		 * to enter an integer and displays its sum.
+		 Write a recursive method that computes the sum of the digits in an integer.
+		 Use the following method header:
+		 
+		 public static int sumDigits(long n)
+		 
+		 For example, sumDigits(234) returns 2 + 3 + 4 = 9.
+		 Write a test program that prompts the user to enter an integer and displays its sum.
 		 */
 		
 		Scanner input = new Scanner(System.in);
+		
 		System.out.print("Unesi broj: ");
-		int n = input.nextInt();
+		
+		int n = input.nextInt();    // unos broja
+		                            // pozivanje metode
 		System.out.println("Suma cifara unesenog broja je: " + sumDigits(n));
 		
 		input.close();
 	}
-	
+	/** Metoda koja prima broj 'n'
+	 * i vraca sumu cifara broja*/
 	public static int sumDigits(long n) {
 		
-		int broj = (int)Math.abs(n);
+		int broj = (int)Math.abs(n); // pretvaramo broj n u broj abs vrijednosti
 		int suma = 0;
 		
-		suma += broj % 10;
+		suma += broj % 10;           // u sumu ubacujemo zadnju cifru broja 
 		
-		if(broj > 0)
-		suma += sumDigits(broj /= 10); 
+		if(broj > 0) {               // ako je broj veci od 0
+			                         // sumiramo brojeve pozivanjem metode (sama sebe - rekurzija)
+			suma += sumDigits(broj /= 10);
+		}
 
-		return suma;
+		return suma;                 // vracanje ukupne sume cifara unesenog broja
 		
 	}
 }
